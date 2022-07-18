cf49677b8f4552c2683cc15ab09e069c4e3010d1
Nam Thai Tran <tran.nam.thai.02@gmail.com>
2022-07-18 21:21:16 +0700

    added: data attributes | working on helper functions

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 import java.util.ArrayList;
 
-public class test {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("here");
         String userFile = "../data/user.csv";
         ArrayList<String[]> dataArr = Helper.readData(userFile);
         for (int i = 0; i < dataArr.size(); i++) {
--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,32 @@
 public class Product {
+    private String productId, productName, category, price;
+
+    public Product(String productId, String productName, String category, String price) {
+        this.productId = productId;
+        this.productName = productName;
+        this.category = category;
+        this.price = price;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+
 }
