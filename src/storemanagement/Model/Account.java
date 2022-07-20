27f35a8dff046e0f2d6e5a25e504ab4493744a71
Dong <phamvodong0811@gmail.com>
2022-07-20 14:29:46 +0700

    Create AccountController.java

--- src/storemanagement/Model/Account.java
+++ src/storemanagement/Model/Account.java
@@ -1,7 +1,13 @@
 package storemanagement.Model;
 
+import storemanagement.Service.Helper;
+
 public class Account {
-    String username, password, fullName, membership;
+//    private String cusID;
+    private final String username;
+    private final String password;
+    private final String fullName;
+    public String membership;
     int phone;
     public Account(String username, String password, String fullName, int phone, String membership){
         this.username = username;
