4c19a95f5d325c9f9ff26efd64699219e8600347
ThuPham02 <phamthu10602@gmail.com>
2022-07-21 16:19:14 +0700

    add generatedID

--- src/storemanagement/Controller/ProductController.java
+++ src/storemanagement/Controller/ProductController.java
@@ -5,13 +5,13 @@ import storemanagement.Service.Helper;
 import java.util.*;
 
 public class ProductController {
-    static String productDataFile = "data/product.csv";
+    String productDataFile = "data/product.csv";
     Scanner inp = new Scanner(System.in);
     ArrayList<String[]> dataArr = Helper.readData(productDataFile);
 
     public static void main(String[] args) {
         ProductController productController = new ProductController();
-        productController.addProduct();
+        productController.addProduct("product3", "test", 100000);
     }
 
     public ProductController() {
@@ -37,7 +37,7 @@ public class ProductController {
         Helper.deleteLine(productDataFile, productId);
     }
 
-    public static boolean productNameValidate(String productName) {
+    public boolean productNameValidate(String productName) {
         ArrayList<String[]> dataArr = Helper.readData(productDataFile);
         for (int i = 1; i < dataArr.size(); i++) {
             String[] line = dataArr.get(i);
@@ -57,35 +57,33 @@ public class ProductController {
     }
 
     // 9. An admin can add a new product to the Store
-    public void addProduct() {
-        try {
-            System.out.println("Enter the Product Name: ");
-            String productName = inp.nextLine();
-            System.out.println("Enter the Category: ");
-            String category = inp.nextLine();
-            System.out.println("Enter the Product Price: ");
-            long price = this.inp.nextLong();
+    public void addProduct(String productName, String category, long price) {
+//            System.out.println("Enter the product name: ");
+//            String productName = inp.nextLine();
+//            System.out.println("Enter the category: ");
+//            String category = inp.nextLine();
+//            System.out.println("Enter the product price: ");
+//            long price = this.inp.nextLong();
             long finalPrice = priceValidate(price);
             String dataAdd = productName + "," + category + "," + finalPrice;
 
             if (productNameValidate(productName)) {
+                System.out.println("Successfully added new product to the store!");
                 Helper.addData(productDataFile, dataAdd);
             } else {
                 System.out.println("The product \"" + productName + "\" already exists in the store!");
             }
 
-        } catch (Exception e) {
-            System.out.println(e);
-        }
     }
 
     // 10. An admin can update price for a particular product
-    public void updatePrice() {
-        System.out.println("Enter the product name that you want to change its price: ");
-        String productName = inp.nextLine();
+    public void updatePrice(String productName, long newPrice) {
+//        System.out.println("Enter the product name that you want to change its price: ");
+//        String productName = inp.nextLine();
+//
+//        System.out.println("Enter the new price: ");
+//        long newPrice = inp.nextLong();
 
-        System.out.println("Enter the new price: ");
-        long newPrice = inp.nextLong();
         long finalPrice = priceValidate(newPrice);
 
         if (!productNameValidate(productName)) {
@@ -103,13 +101,22 @@ public class ProductController {
     }
 
     // 5. A customer can sort all products by product price
-    public void sortProduct() {
+    public void sortProduct(String input) {
         ArrayList<String> priceArr = new ArrayList<>();
         for (int i = 1; i < dataArr.size(); i++) {
-            String[] price = dataArr.get(i);
-            priceArr.add(price[3]);
+            String[] line = dataArr.get(i);
+            priceArr.add(line[3]);
         }
-        Collections.sort(priceArr);
+
+//        System.out.println("Enter A for ascending order or D for descending order: ");
+//        String productName = inp.nextLine();
+
+        if (input.equalsIgnoreCase("A")) {
+            Collections.sort(priceArr);
+        } else if (input.equalsIgnoreCase(("D"))) {
+            Collections.sort(priceArr, Collections.reverseOrder());
+        }
+
         for (int j = 0; j < priceArr.size(); j++) {
             for (int i = 0; i < dataArr.size(); i++) {
                 String[] line = dataArr.get(i);
@@ -121,20 +128,21 @@ public class ProductController {
     }
 
     // 4. A customer can search for all available products for a particular category
-    public void searchProduct() {
-        System.out.println("Enter the product name you want to view: ");
-        String productName = inp.nextLine();
-
+    public String searchProduct(String productName) {
+//        System.out.println("Enter the product name you want to view: ");
+//        String productName = inp.nextLine();
+        String product = "";
         if (!productNameValidate(productName)) {
             for (int i = 0; i < dataArr.size(); i++) {
                 String[] line = dataArr.get(i);
                 if (productName.equalsIgnoreCase(line[1])) {
-                    System.out.println("Product name: " + line[1] + "\nCategory: " + line[2] + "\nPrice: " + line[3]);
+                    product =  "Product name: " + line[1] + "\nCategory: " + line[2] + "\nPrice: " + line[3];
                 }
             }
         } else {
-            System.out.println("Product \"" + productName + "\" does not exist.");
+            product = "Product \"" + productName + "\" does not exist.";
         }
+        return product;
     }
 
     public ArrayList<String[]> getDataArr() {
