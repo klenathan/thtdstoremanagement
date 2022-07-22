c07ab9d8d23836401de7b9762cc479fa6e53290a
ThuPham02 <phamthu10602@gmail.com>
2022-07-22 19:11:49 +0700

    update OrderController

--- src/storemanagement/Controller/OrderController.java
+++ src/storemanagement/Controller/OrderController.java
@@ -1,42 +1,80 @@
 package storemanagement.Controller;
 
+import com.sun.tools.javac.Main;
 import storemanagement.Model.Order;
 import storemanagement.Service.Helper;
 
 import java.util.ArrayList;
-import java.util.Objects;
 
 public class OrderController {
 
+    public static void main(String[] args) {
+        OrderController orderController = new OrderController();
+        orderController.updateOrderStatus("O1");
+    }
     private final String orderDataFile = "data/order.csv";
-    ArrayList<String[]> dataArr;
+    private ArrayList<String[]> dataArr;
 
     public OrderController() {
-        dataArr = Helper.readData(orderDataFile);
+        this.dataArr = Helper.readData(orderDataFile);
     }
 
-    public void createOrder(String productId, String userId, int quantity){
+    public void createOrder(String productId, String userId, int quantity, long price){
+        long totalBill = quantity * price;
 
-        String orderId = "a";
-        long totalBill = quantity * 100000L;
-
-        String orderDetail = orderId + "," + productId
+        String orderDetail = productId
                 + "," + userId + "," + quantity + "," + totalBill + "," + "UNPAID";
-        Helper.addData(this.orderDataFile, orderDetail);
+        Helper.addData(orderDataFile, orderDetail);
     }
 
     public void updateOrderStatus(String orderId) {
 //        String tempData;
         if(Helper.getAllId(orderDataFile).contains(orderId)) {
-            for(String[] line: dataArr){
-                if(Objects.equals(line[0], orderId)){
-                    String newData = line[0] + ',' + line[1] + "," + line[2]
-                            + "," + line[3] + ',' + line[4] + "," + "PAID";
-                    Helper.deleteLine(orderDataFile, orderId);
-                    Helper.addData(orderDataFile, newData);
-                }
-            }
+            Helper.modifyField(orderDataFile, orderId,5, "PAID");
+        } else {
+            System.out.println("The product ID \"" + orderId + "\" does not exists in the store!");
         }
     }
 
+    public String membershipCheck(String customerID) {
+        long totalBill = 0;
+        String membership;
+        for (String[] line: dataArr) {
+            if (customerID.equalsIgnoreCase(line[2]) && line[5].equalsIgnoreCase("paid")) {
+                totalBill += Integer. parseInt(line[4]);
+            }
+        }
+
+        if (totalBill > 25000000) {
+            membership = "Platinum";
+        } else if (totalBill > 10000000) {
+            membership = "Gold";
+        } else if (totalBill > 5000000) {
+            membership = "Silver";
+        } else {
+            membership = "None";
+        }
+        return membership;
+    }
+
+
+    public double membershipDiscount(String customerID) {
+        String membership = membershipCheck(customerID);
+        double discount;
+        if (membership.equalsIgnoreCase("Silver")) {
+            discount = 0.05;
+        } else if (membership.equalsIgnoreCase("Gold")) {
+            discount = 0.1;
+        } else if (membership.equalsIgnoreCase("Platinum")) {
+            discount = 0.15;
+        } else {
+            discount = 0;
+        }
+        return discount;
+    }
+
+
+    public void listAllOrder() {
+        Helper.listAll(orderDataFile);
+    }
 }
