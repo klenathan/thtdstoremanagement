2f3818c6a8050ac77baa5bdbe71f94a6870c891e
ThuPham02 <phamthu10602@gmail.com>
2022-08-04 21:50:47 +0700

    small changes

--- src/storemanagement/Controller/ProductController.java
+++ src/storemanagement/Controller/ProductController.java
@@ -131,11 +131,11 @@ public class ProductController {
 
         HashMap<String, String[]> a = new HashMap<>();
         ArrayList<String[]> res = new ArrayList<>();
-        for (Long aLong : priceArr) {
-            for (int i = 1; i < dataArr.size(); i++) {
-                if (Long.parseLong(dataArr.get(i)[3]) == priceArr.get(i) && !a.containsKey(dataArr.get(i)[0])) {
-                    a.put(dataArr.get(i)[0], dataArr.get(i));
-                    res.add(a.get(dataArr.get(i)[0]));
+        for (int i = 0; i < priceArr.size(); i++) {
+            for (int j = 1; j < dataArr.size(); j++) {
+                if (Long.parseLong(dataArr.get(j)[3]) == priceArr.get(i) && !a.containsKey(dataArr.get(j)[0])) {
+                    a.put(dataArr.get(j)[0], dataArr.get(j));
+                    res.add(a.get(dataArr.get(j)[0]));
                 }
             }
         }
--- src/storemanagement/View/Menu.java
+++ src/storemanagement/View/Menu.java
@@ -374,8 +374,8 @@ public class Menu {
         int colWidth = 30;
         for (String[] line : displayData) {
             for (int j = 0; j < line.length; j++) {
-                if (line[j].length() > 15) {
-                    line[j] = line[j].substring(0, Math.min(line[j].length(), 12)) + "...";
+                if (line[j].length() > 30) {
+                    line[j] = line[j].substring(0, Math.min(line[j].length(), 27)) + "...";
                 }
                 System.out.print(" " + line[j] + " ".repeat(colWidth - line[j].length()) + "||");
             }
